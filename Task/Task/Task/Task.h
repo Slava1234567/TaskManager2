bd99f61a9e6691e01a005aed694d0c3754686993
alspiat <alspiat@icloud.com>
2018-05-17 02:47:04 +0300

    Update TasksViewController with protocol AddTaskViewControllerDelegate implementation and update AddTaskViewController with test field and delegate

---
+++ Task/Task/Task/Task.h
@@ -0,0 +1,17 @@
+//
+//  Task.h
+//  Task
+//
+//  Created by Алексей on 17.05.2018.
+//  Copyright © 2018 Slava. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Task : NSObject
+
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) NSString *descript;
+@property (nonatomic, retain) NSString *details;
+
+@end
---
+++ Task/Task/Task/Task.m
@@ -0,0 +1,13 @@
+//
+//  Task.m
+//  Task
+//
+//  Created by Алексей on 17.05.2018.
+//  Copyright © 2018 Slava. All rights reserved.
+//
+
+#import "Task.h"
+
+@implementation Task
+
+@end
